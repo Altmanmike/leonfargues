dbdaf7e2ce995f943dcbb6c52b9f6ea98e0911fb
Michael ALTMAN <altman_mike@yahoo.fr>
2026-01-07 16:38:57 +0100

    ADD film controller and form film

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Film;
+use App\Form\FilmFormType;
 use App\Repository\FilmRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,6 +21,67 @@ final class FilmController extends AbstractController
         return $this->render('film/index.html.twig', [
             'controller_name' => 'FilmController',
             'film' => $film
+        ]);
+    }
+
+    #[Route('/supprimerFilm/{id}', name: 'app_supprimer_film')]
+    public function supprimerFilm(FilmRepository $repoF, $id, EntityManagerInterface $em): Response
+    {        
+        try {
+            if ($repoF->find($id)) {
+               $film = $repoF->find($id);
+                $em->remove($film);
+                $em->flush();
+                
+                $this->addFlash('success', 'Film retiré.');
+                return $this->redirectToRoute('app_accueil'); 
+            }            
+
+        } catch (\Exception $e) {
+            $this->addFlash('error', $e->getMessage());
+            return $this->redirectToRoute('app_accueil');
+        }
+                
+        return $this->redirectToRoute('app_accueil');
+    }
+
+    #[Route('/ajouterFilm', name: 'app_ajouter_film')]
+    public function ajouterFilm(EntityManagerInterface $em, Request $request): Response
+    {
+        $film = new Film();
+        $form = $this->createForm(FilmFormType::class, $film);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            try {
+                $film->setTitre(htmlspecialchars(trim($form->get('titre')->getData())));
+                $film->setAnnee(htmlspecialchars(trim($form->get('annee')->getData())));
+                $film->setImage(htmlspecialchars(trim($form->get('image')->getData())));
+                $film->setSynopsis(htmlspecialchars(trim($form->get('synopsis')->getData())));
+
+                $film->setRealisateur($form->get('realisateur')->getData());
+                $film->setGenre($form->get('genre')->getData());
+                              
+                $film->setCreatedAt(new \DatetimeImmutable());
+                $film->setUpdatedAt(new \DatetimeImmutable());                
+                
+                $em->persist($film);
+                $em->flush();
+                $this->addFlash('success', 'Product saved.');
+
+                return $this->redirectToRoute('app_home');
+                
+            } catch (\Exception $e) {
+                $this->addFlash('error', $e->getMessage());
+                
+                return $this->redirectToRoute('app_home');               
+            }
+        }
+        
+        return $this->render('film/ajouterFilm.html.twig', [
+            'controller_name' => 'FilmController',            
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/FilmFormType.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Film;
+use App\Entity\Genre;
+use App\Entity\Realisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+class FilmFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Titre du film'
+                ],
+                'label' => 'Titre',
+                'label_attr' => ['class' => 'my-2']
+            ])
+            ->add('annee', NumberType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Annéee de sortie'
+                ],
+                'label' => 'Année',
+                'label_attr' => ['class' => 'my-2']
+            ])
+            ->add('image', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Couverture du film'
+                ],
+                'label' => 'Couverture',
+                'label_attr' => ['class' => 'my-2']
+            ])
+            ->add('synopsis', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Synopsis du film'
+                ],
+                'label' => 'Synopsis',
+                'label_attr' => ['class' => 'my-2']
+            ])
+            ->add('createdAt', null, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Date d\'ajout'
+                ],
+                'label' => 'Ajouté le',
+                'widget' => 'single_text',
+                'label_attr' => ['class' => 'my-2']
+            ])
+            ->add('updatedAt', null, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Date de modification'
+                ],
+                'label' => 'Mis à jour le',
+                'widget' => 'single_text',
+                'label_attr' => ['class' => 'my-2']
+            ])
+            ->add('realisateur', EntityType::class, [                
+                'attr' => [
+                    'class' => 'form-control'                    
+                ],
+                'class' => Realisateur::class,
+                'choice_label' => 'id',
+                'label_attr' => ['class' => 'my-2']
+            ])
+            ->add('genre', EntityType::class, [                
+                'attr' => [
+                    'class' => 'form-control mb-4'                    
+                ],
+                'class' => Genre::class,
+                'choice_label' => 'id',
+                'label_attr' => ['class' => 'my-2']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Film::class,
+        ]);
+    }
+}
